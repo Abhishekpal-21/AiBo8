224434c83cb1cf6e66adfdcf822251ae5cebdb88
AnishGzp <anishsinghgzp1024@gmail.com>
2026-06-13 12:55:22 +0530

    nav responsive fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
 
   return (
     <header className="">
